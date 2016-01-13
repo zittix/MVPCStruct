11189bcbcfc64207fcd6fae9ebb378f5b8f49197
Mathieu Monney <zittix@xwaves.net>
2016-01-13 12:31:39 +0100

    Added missing header for framework

---
+++ MVPCStruct/MVPCStruct.h
@@ -0,0 +1,18 @@
+//
+//  MVPCStruct.h
+//  MVPCStruct
+//
+//  Created by Per Olofsson on 2014-06-13.
+//  Copyright (c) 2014 AutoMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MVPCStruct.
+FOUNDATION_EXPORT double MVPCStructVersionNumber;
+
+//! Project version string for MVPCStruct.
+FOUNDATION_EXPORT const unsigned char MVPCStructVersionString[];
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <MVPCStruct/PublicHeader.h>
